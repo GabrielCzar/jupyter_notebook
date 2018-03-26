fd8bbdf1fb9400d5b41bf2170ccac109ea7fea2f
Gabriel Cesar <gcal1@live.com>
2018-03-26 11:11:45 -0300

    correacao library

--- Library/linear_model.py
+++ Library/linear_model.py
@@ -1,19 +1,30 @@
 #-*- coding=utf-8 -*-
-import linear_regression as lr
+import numpy as np
+
 
 class SimpleLinearRegression:
+            
+    def __isNumpyArray(self, arr):
+        return type(arr) == np.ndarray
     
-    def __init__(self):
-        '''Linear model'''
-        self.b0 = 0
-        self.b1 = 0
-        
     def fit(self, x, y):
         '''Treina o model baseado no gradiente descendente'''
-        self.b0, self.b1 = lr.gradient_descendent(x, y, learning_rate=0.00000001, num_iterations=100000)
+        if self.__isNumpyArray(x):
+            x = x[:, 0]
+            
+        b1 = np.sum((x - np.mean(x)) * (y - np.mean(y))) / np.sum((x - np.mean(x)) ** 2) 
+        b0 = np.mean(y) - b1 * np.mean(x)
+        print ('b0={}, b1={}'.format(b0, b1))
+        
+        self.b0, self.b1 = b0, b1
     
     def predict(self, x):
         '''Cria a predição dos valores de x'''
+        if self.__isNumpyArray(x):
+            x = x[:, 0]
+            
+        print('b0={}, b1={}'.format(self.b0, self.b1))
+        
         return self.b0 + self.b1 * x
         
     
